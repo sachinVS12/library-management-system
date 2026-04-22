d8cbf2f4e9d244c797e8026bc02594962cad0075
Sachin V S <vs1sachi12@gmail.com>
2026-04-22 11:41:31 +0530

    authroutes upadated

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,13 +3,18 @@ const {
   registerUser,
   loginUser,
   getMe,
+  updateProfile,
+  changePassword,
 } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
+const { validateRegister, checkValidation } = require("../utils/validation");
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", validateRegister, checkValidation, registerUser);
 router.post("/login", loginUser);
 router.get("/me", protect, getMe);
+router.put("/update-profile", protect, updateProfile);
+router.put("/change-password", protect, changePassword);
 
 module.exports = router;
